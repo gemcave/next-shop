5c60a0bcac6b558e7f9adb7799a6c74cd00a7030
yerowyn <yerowyn@gmail.com>
2020-03-26 00:50:33 +0600

    add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.h1`
+	font-size: 2em;
+	text-align: left;
+	padding: 16px;
+`
 const Index = (props) => {
 	return (
 		<div>
-			<p>Index</p>
+			<StyledHeader>Index</StyledHeader>
 		</div>			
 		);
 }
