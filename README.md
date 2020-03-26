88cbff2e5a78780ade90305921dfaa7fe34c83b8
yerowyn <yerowyn@gmail.com>
2020-03-26 14:53:13 +0600

    add MainLayout component

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-next-shop
+# next-shop
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
+import { MainLayout } from '../components/layout/MainLayout';
 
 const StyledHeader = styled.h1`
-	font-size: 2em;
-	text-align: left;
-	padding: 16px;
-`
+	${({theme}) => `	 
+		font-size: 2em;
+		text-align: left;
+		padding: ${theme['padding-small']} ${theme['padding-small']};
+	`}
+`;
+
 const Index = (props) => {
 	return (
-		<div>
+		<MainLayout>
 			<StyledHeader>Index</StyledHeader>
-		</div>			
+		</MainLayout>			
 		);
 }
 
