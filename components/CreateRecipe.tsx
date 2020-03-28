cf5e6698cf15ffa2384ca73f0f4276c8ca12866b
yerowyn <yerowyn@gmail.com>
2020-03-28 22:37:07 +0600

    add form text area

---
+++ components/CreateRecipe.tsx
@@ -0,0 +1,48 @@
+import { Form, Row, Col, Button } from "antd";
+import { submitForm } from "../utils/submitForm";
+import { GenerateInput, GenerateTextInput } from "./GenerateFields";
+
+export const CreateRecipe = () => {
+  const initiateCreateRecipe = () => {
+    console.log("submitted Form");
+    console.log(inputs);
+  };
+
+  const { inputs, handleInputChange, handleSubmit } = submitForm(
+    {
+      title: "",
+      description: "",
+      content: ""
+    },
+    initiateCreateRecipe
+  );
+  return (
+    <Form onFinish={handleSubmit}>
+      <GenerateInput
+        name="title"
+        value={inputs.title}
+        handleInputChange={handleInputChange}
+      />
+      <GenerateInput
+        name="description"
+        value={inputs.description}
+        handleInputChange={handleInputChange}
+      />
+      <GenerateTextInput
+        name="content"
+        value={inputs.content}
+        handleInputChange={handleInputChange}
+      />
+      <Row>
+        <Col span={16} />
+        <Col span={4}>
+          <Form.Item label="Create Recipe">
+            <Button type="primary" htmlType="submit">
+              Create Recipe
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
---
+++ utils/submitForm.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import * as _ from "lodash";
+
+export const submitForm = (initialValues, callback) => {
+  const [inputs, setInputs] = useState(initialValues);
+
+  const handleSubmit = event => {
+    if (event) event.preventDefault();
+    callback();
+    setInputs(() => ({ ...initialValues }));
+  };
+
+  const handleInputChange = event => {
+    event.persist();
+    setInputs(inputs => {
+      const newInputs = _.cloneDeep(inputs);
+      _.set(newInputs, event.target.name, event.target.value);
+      return newInputs;
+    });
+  };
+
+  return {
+    inputs,
+    handleInputChange,
+    handleSubmit
+  };
+};
